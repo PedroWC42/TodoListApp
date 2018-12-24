679c76b05452cb6635594ab776a84fd1f57a7162
José Villanueva Rojas <macpedrowc38@mac-mini.local>
2018-12-24 06:54:26 -0600

    Initial Commit

---
+++ TodoListApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TodoListApp
+//
+//  Created by José Villanueva Rojas on 12/24/18.
+//  Copyright © 2018 PedroWC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
